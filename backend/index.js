570cde575966ca6426353cfe230892f57bfd5b8e
Ramazan <108758151+inuazh@users.noreply.github.com>
2024-07-22 12:15:11 +0300

    mvp

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { default: axios } = require("axios");
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -8,20 +8,20 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  
+
   try {
     const r = await axios.put(
-      'https://api.chatengine.io/users/',
-        {username: username,secret: username, first_name}
-    )
-  } catch (error) {
-    
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "fe93fdf1-3756-47cc-b155-08ec9cd47dc8" } }
+    );
+
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
   }
 
-  fe93fdf1-3756-47cc-b155-08ec9cd47dc8
-  return res.json({ username: username, secret: "sha256..." });
+  // return res.json({ username: username, secret: "sha256..." });
 });
-
-
 
 app.listen(3001);
